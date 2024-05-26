96b5bf17a75f103f8984d1ecd4983c1068ad1b1b
Deyvids <deyvidsalvino@gmail.com>
2024-05-26 16:57:28 -0500

    [NEW] Creando controller de servicios

---
+++ src/main/java/com/restaurant/system/backend_restaurant_system/controller/OrderController.java
@@ -0,0 +1,10 @@
+package com.restaurant.system.backend_restaurant_system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/order")
+public class OrderController {
+    
+}
